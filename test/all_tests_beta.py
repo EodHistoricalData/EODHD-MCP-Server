3b9abe061552aba8b11244d594847e67884b3434
brmn <elesin@gmail.com>
2025-12-12 12:23:03 +0200

    New chapter

---
+++ test/all_tests_beta.py
@@ -0,0 +1,20 @@
+# This module registers all "new" tests cases to facilitate debugging only new features.
+
+def register(add_test, COMMON):
+    pass
+# --- Praams Bank Income Statement by ISIN ---
+#    add_test({
+#        "name": "Praams Bank Balance Sheet retrieval by ISIN (US46625H1005)",
+#        "tool": "get_mp_praams_bank_balance_sheet_by_isin",
+#        "use_common": ["api_token"],
+#        "params": {
+#            "isin": "US46625H1005",
+#        },
+#    })
+
+
+
+
+
+
+
